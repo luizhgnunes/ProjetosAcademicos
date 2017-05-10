33d789e56a117e156698282d602ca73ced614cc6
Gabriel Alves <ggabriell455@gmail.com>
2017-05-10 20:10:19 -0300

    Teste de commit.

--- SmallRoad/Branch/WebSite/SmallRoadWeb/Startup.cs
+++ SmallRoad/Branch/WebSite/SmallRoadWeb/Startup.cs
@@ -8,7 +8,7 @@ namespace SmallRoadWeb
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app); // teste de commit
         }
     }
 }
