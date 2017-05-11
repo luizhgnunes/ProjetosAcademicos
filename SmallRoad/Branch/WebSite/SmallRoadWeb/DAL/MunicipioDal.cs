6609d0eeea46e626cb97ea275a168173a6d17e33
Rodrigo Carvalho <roda7x@gmail.com>
2017-05-11 01:34:04 -0300

    Criado metodo Deletar e EncontrarMunicipioPorId na classe MunicipioDal.

--- SmallRoad/Branch/WebSite/SmallRoadWeb/DAL/MunicipioDal.cs
+++ SmallRoad/Branch/WebSite/SmallRoadWeb/DAL/MunicipioDal.cs
@@ -7,21 +7,18 @@ namespace SmallRoadWeb.DAL
 {
     public class MunicipioDal
     {
+
+        public SqlConnection conn;
+
         public MunicipioDal()
         {
-            ConectarBanco();
-        }
-
-        public SqlConnection ConectarBanco()
-        {
-            return Connection.GetConnection();
-
+            conn = Connection.GetConnection();
         }
 
         public bool Cadastrar(Municipio municipio)
         {
             String query = @"INSERT INTO TB_MUNICIPIO (EST_UF,MUN_NOME) VALUES (@Uf, @Nome);";
-            SqlCommand cmd = new SqlCommand(query, ConectarBanco());
+            SqlCommand cmd = new SqlCommand(query, conn);
             cmd.Parameters.AddWithValue("@Uf", municipio.Uf);
             cmd.Parameters.AddWithValue("@Nome", municipio.Nome);
             int lines = cmd.ExecuteNonQuery(); 
@@ -34,10 +31,62 @@ namespace SmallRoadWeb.DAL
             {
                 return false;
             }
+        }
 
 
+        public bool Atualizar(Municipio municipio)
+        {
+            String query = @"UPDATE TB_MUNICIPIO SET (EST_UF= @Uf,NUM_NOME= @Nome) where (MUN_ID = @Id);";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Id", municipio.Id);
+            cmd.Parameters.AddWithValue("@Uf", municipio.Uf);
+            cmd.Parameters.AddWithValue("@Nome", municipio.Nome);
+            int lines = cmd.ExecuteNonQuery();
+
+            if (lines > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 
         }
+
+        public bool Deletar(Municipio municipio)
+        {
+            String query = @"DELETE FROM TB_MUNICIPIO WHERE (MUN_ID = @Id);";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Id", municipio.Id);
+            int lines = cmd.ExecuteNonQuery();
+
+            if (lines > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public Municipio EncontrarMunicipioPorId(int id)
+        {
+            Municipio municipio = new Municipio();
+            String query = @"SELECT * TB_MUNICIPIO WHERE (MUN_ID = @Id);";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                cmd.Parameters["@ID"].Value = municipio.Id;
+                cmd.Parameters["@Uf"].Value = municipio.Uf;
+                cmd.Parameters["@Nome"].Value = municipio.Nome;
+                return municipio;
+            }
+
+            return null;
+         }
 
 
 
