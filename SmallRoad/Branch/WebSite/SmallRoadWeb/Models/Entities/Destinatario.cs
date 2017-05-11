26c66d72abee2cb046e313d9820856cb8bf398fe
Rodrigo Carvalho <roda7x@gmail.com>
2017-05-11 00:36:59 -0300

    Entidades Destinatario e Municipio adicionadas.

---
+++ SmallRoad/Branch/WebSite/SmallRoadWeb/Models/Entities/Destinatario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmallRoadWeb.Models.Entities
+{
+    public class Destinatario
+    {
+
+        public int Numero { get; set; }
+        public String Nome { get; set; }
+        public String Cpf { get; set; }
+        public String Latitude { get; set; }
+        public String Longitude { get; set; }
+        public String Endereço { get; set; }
+        public String Bairro { get; set; }
+        public String Cep { get; set; }
+        public Municipio Municipio { get; set; }
+        public int Ddd { get;set; }
+        public int Telefone { get; set; }
+        public DateTime DtCadastro { get; set; }
+
+
+
+
+    }
+}
---
+++ SmallRoad/Branch/WebSite/SmallRoadWeb/Models/Entities/Municipio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmallRoadWeb.Models.Entities
+{
+    public class Municipio
+    {
+        public int id { get; set; }
+        public String uf {get;set;}
+        public String Nome { get; set; }
+    }
+}
